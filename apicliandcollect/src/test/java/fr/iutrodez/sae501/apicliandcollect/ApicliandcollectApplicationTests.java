e91e327f917da571368f5cc6e32347421a6fb6be
LucasDescriaud <lucas.descriaud@iut-rodez.fr>
2024-11-25 16:40:26 +0100

    add default files

---
+++ apicliandcollect/src/test/java/fr/iutrodez/sae501/apicliandcollect/ApicliandcollectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iutrodez.sae501.apicliandcollect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicliandcollectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
