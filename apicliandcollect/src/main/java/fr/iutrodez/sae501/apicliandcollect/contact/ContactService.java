843feee6eed6c90754be2ed62681425744074054
Loïc Faugières <loic.faugieres@gmail.com>
2025-03-19 17:29:49 +0100

    fix(client): méthodes get client & prospect proche avec x et y en param

--- apicliandcollect/src/main/java/fr/iutrodez/sae501/apicliandcollect/contact/ContactService.java
+++ apicliandcollect/src/main/java/fr/iutrodez/sae501/apicliandcollect/contact/ContactService.java
@@ -38,9 +38,6 @@ public class ContactService {
     @Autowired
     private InteractionMongoItineraire interactionMongoItineraire;
 
-    @Autowired
-    private InteractionMongoUtilisateur interactionMongoUtilisateur;
-
     /**
      * Crée un nouveau contact pour l'utilisateur u
      *
@@ -133,29 +130,32 @@ public class ContactService {
 
     /**
      * Récupère la liste des prospects à moins de 1000m de l'utilisateur.
-     * @param u L'utilisateur connecté
+     * @param longitude La longitude à comparer
+     * @param latitude La latitude à comparer
      * @return La liste des prospects proches
      */
-    public List<ContactDTO> getProspectsProches(Utilisateur u) {
-        GeoJsonPoint localisation = interactionMongoUtilisateur.findBy_id(u.getId()).getLocation();
+    public List<ContactDTO> getProspectsProches(double longitude, double latitude) {
+        GeoJsonPoint localisation = new GeoJsonPoint(longitude, latitude);
 
         List<ContactMongo> prospectsProches
         = interactionMongoContact.findByLocationNear(localisation, DISTANCE_PROSPECT_PROCHE);
 
-        return prospectsProches.stream().map(prospect -> {
-            Contact contact = interactionBdContact.findById(prospect.get_id()).get();
-            return contactEnJson(contact, prospect);
-        }).collect(Collectors.toList());
+        return prospectsProches.stream()
+            .map(prospect -> interactionBdContact.findById(prospect.get_id()).get())
+            .filter(Contact::isProspect)  // Vérifie que le contact est bien un prospect
+            .map(contact -> contactEnJson(contact, interactionMongoContact.findBy_id(contact.getId())))
+            .collect(Collectors.toList());
     }
 
     /**
      * Vérifie si le client est proche de l'utilisateur
-     * @param u L'utilisateur connecté
      * @param idClient L'id du client
+     * @param latitude La latitude à comparer
+     * @param longitude La longitude à comparer
      * @return true si le client est proche, false sinon
      */
-    public boolean isClientProche(Utilisateur u, long idClient) {
-        GeoJsonPoint localisation = interactionMongoUtilisateur.findBy_id(u.getId()).getLocation();
+    public boolean isClientProche(long idClient, double longitude, double latitude) {
+        GeoJsonPoint localisation = new GeoJsonPoint(longitude, latitude);
 
         List<ContactMongo> clientsProches = interactionMongoContact.findByLocationNear(localisation, DISTANCE_CLIENT_PROCHE);
 
--- apicliandcollect/src/main/java/fr/iutrodez/sae501/apicliandcollect/contact/ControleurContact.java
+++ apicliandcollect/src/main/java/fr/iutrodez/sae501/apicliandcollect/contact/ControleurContact.java
@@ -47,21 +47,27 @@ public class ControleurContact {
 
     /**
      * Récupère la liste des prospects proches de l'utilisateur connecté.
-     * @param utilisateur L'utilisateur connecté
+     * @param longitude La longitude à comparer
+     * @param latitude La latitude à comparer
      * @return Une entité de réponse contenant la liste des prospects proches de l'utilisateur
      */
     @GetMapping("/contact/prospect/proche")
-    public ResponseEntity<List<ContactDTO>> obtenirProspectsProches(Authentication utilisateur) {
-        Utilisateur u = (Utilisateur) utilisateur.getPrincipal();
-        List<ContactDTO> contact = service.getProspectsProches(u);
-        return new ResponseEntity<>(contact, HttpStatus.OK);
+    public ResponseEntity<List<ContactDTO>> obtenirProspectsProches(@RequestParam double longitude,
+                                                                    @RequestParam double latitude) {
+        return new ResponseEntity<>(service.getProspectsProches(longitude, latitude), HttpStatus.OK);
     }
 
+    /**
+     * Vérifie si un client est proche de l'utilisateur connecté.
+     * @param id L'identifiant du client
+     * @param longitude La longitude à comparer
+     * @param latitude La latitude à comparer
+     * @return Une entité de réponse indiquant si le client est proche ou non
+     */
     @GetMapping("/contact/client/{id}/proche")
-    public ResponseEntity<ReponseEstProche> estClientProche(@PathVariable Long id, Authentication utilisateur) {
-        Utilisateur u = (Utilisateur) utilisateur.getPrincipal();
-
-        return new ResponseEntity<>(new ReponseEstProche(service.isClientProche(u, id)),
+    public ResponseEntity<ReponseEstProche> estClientProche(@PathVariable Long id, @RequestParam double longitude,
+                                                            @RequestParam double latitude) {
+        return new ResponseEntity<>(new ReponseEstProche(service.isClientProche(id, longitude, latitude)),
                                     HttpStatus.OK);
     }
 
